797b8330ea1017603b312f56efb6d8ebe6c82e36
AndrasKovacs <puttamalac@gmail.com>
2024-10-11 18:03:43 +0200

    wip

--- runtime_codegen/proto2/rts.js
+++ runtime_codegen/proto2/rts.js
@@ -4,6 +4,8 @@ function impossible(){
     throw new Error('Impossible')
 }
 
+// ADT for open values
+
 // const _Var    = 0
 // const _Let    = 1
 // const _Lam    = 2
@@ -49,30 +51,113 @@ function Write_  (t, u)    {return {tag: _Write, _1: t, _2: u}}
 function Read_   (t)       {return {tag: _Read, _1: t}}
 function Closed_ (t)       {return {tag: _Closed, _1: t}}
 
+
+// Closure conversion
+// --------------------------------------------------------------------------------
+
+const _Closure   = 'Closure'
+const _CSP       = 'CSP'
+const _LiftedLam = 'LiftedLam'
+const _Body      = 'Body'
+
+// data Top
+//   = TLet Name Tm Top
+//   | TBind Name Tm Top
+//   | TSeq Tm Top
+//   | TBody Tm
+//   | TClosure Name [Name] Name Tm Top -- name, env, arg, body
+//   deriving Show
+
+// data Tm
+//   = Var Name
+//   | CSP Name
+//   | Let Name Tm Tm
+//   | LiftedLam Name [Name] -- name, env application
+//   | Lam Name Tm
+//   | App Tm Tm
+//   | Erased String
+//   | Quote Tm
+//   | Splice Tm (Maybe SourcePos)
+//   | Return Tm
+//   | Bind Name Tm Tm
+//   | Seq Tm Tm
+//   | New Tm
+//   | Write Tm Tm
+//   | Read Tm
+//   deriving Show
+
+// type TopClosures = Array {name: String, env: Array String, arg: String, body: Tm}
+// type CCEnv       = Array {isClosed: Bool, isTop: Bool, name: String}
+// type CCConfig    = {env: CCEnv, mode: Int | undefined, topName: String}
+
+// WE SHALL MAKE all the env's and configs mutable top-level!!!!
+
+// CCConfig -> String -> String
+function freshenName(c, x) {
+    if (c.env.findIndex((e) => e.name === x) === -1) {
+	return x
+    } else {
+	return freshenName(c, x + c.env.length)
+    }
+}
+
+{
+    const env  = [{isClosed: true, isTop: true, name: 'mallac'}]
+    const conf = {env: env, mode: undefined, topName: 'foo'}
+    console.log(freshenName(conf, 'mallac'))
+}
+
+// CCConfig -> Name -> (CCConfig -> Name -> a) -> a
+function fresh(c, x, act) {
+    const x2 = freshenName(c, x)
+    c.env.push({isClosed: false, isTop: false, name: x2 })
+    const a = act(c, x2)
+    c.env.pop()
+    return a
+}
+
+// CCConfig ->
+function bind(
+
+
+// -- create fresh name, run action, delete bound name from freevars of the result
+// bind :: Name -> (Env => Name -> State S a) -> Env => State S a
+// bind x act = fresh x \x -> do
+//   a <- act x
+//   s <- get
+//   freeVars %= S.delete x
+//   pure a
+
+
+
+// --------------------------------------------------------------------------------
+
 function openApp_(t, u) {
-    if (t.tag == _Closed) {
-        if (u.tag == _Closed) {
+    if (t.tag === _Closed) {
+        if (u.tag === _Closed) {
             return Closed_(t._1._1(u._1))
         } else {
             return t._1._2(u)
         }
-    } else if (t.tag == _Lam) {
+    } else if (t.tag === _Lam) {
         return t._1(u)
     } else {
         impossible()
     }
 }
 
-function openSplice0_(t) {
-    throw new Error('code generation not implemented')
-}
-
 function openSplice_(t) {
-    if (t.tag == _Quote){
+    if (t.tag === _Closed){
+        return t._1._1
+    } else if (t.tag === _Quote) {
         return t._1
     } else {
-        return Splice_(t)
+	return Splice_(t)
     }
+}
+
+function openSplice0_(t) {
+    throw new Error('code generation not implemented')
 }
 
 function closedExec_(t){
@@ -82,3 +167,30 @@ function closedExec_(t){
 function closedEval_(t){
     throw new Error('code generation not implemented')
 }
+
+// --------------------------------------------------------------------------------
+
+const foo = Lam_('x', (x) => {return x});
+const bar = Lam_('x', (x) => {return App_(openSplice_(Closed_(foo)), x)});
+const main_ = () => {return undefined};
+console.log(main_())
+
+/*
+
+to : {A : Set} → (A → Set) → (B : Set) * (B → A)
+to f = ((a : A) * f a, fst)
+
+from : {A : Set} → ((B : Set) * (B → A)) → (A → Set)
+from (B, f) a = (b : B) * (f b = a)
+
+(A B C : Set) * (C → B) * (B → A)                      ~ (reorder components)
+(A : Set) * (B : Set) * (B → A) * (C : Set) * (C → B)  ~ (apply "to")
+(A : Set) * (B : Set) * (B → A) * (B → Set)             ~ (associativity of Σ)
+(A : Set) * (Bf : (B : Set) * (B → A)) * (fst Bf → Set) ~ (apply "from")
+(A : Set) * (B : A → Set) * (fst (to B) → Set)           =
+(A : Set) * (B : A → Set) * (((a : A) * B a) → Set)      ~ (distribution of Π and Σ)
+(A : Set) * (A → (B : Set) * (B → Set))
+
+
+
+*/
